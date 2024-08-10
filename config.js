013e45954261e54628a1e31a79c327fec9d4284c
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-08-10 22:17:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/e1805cc111530fe514728.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello!* . This is Thenu-MD Whatsapp WA Bot Created By > Thenula Panapiti ❤️💀.. *I'M ALIVE NOW 🔔",
+
 };
