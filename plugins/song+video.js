e5afe102cf1fb2da6a05a7a6c0e6b06aac506830
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-08-17 10:44:12 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -18,11 +18,11 @@ const url = data.url
 let desc = `
   *♻️ 🎶Thenu-MD Song Downloader🎵 ♻️*
   
-  title: ${data.title}
-  description: ${data.description}
-  time: ${data.timestamp}
-  ago: ${data.ago}
-  views: ${data.views}
+  Title: ${data.title}
+  Description: ${data.description}
+  Time: ${data.timestamp}
+  Ago: ${data.ago}
+  Views: ${data.views}
     
  🔔_song  downloading.._
 >© Made by Thenula Panapiti.
@@ -34,13 +34,62 @@ let desc = `
  let down = await fg.yta(url)
  let downloadUrl = down.dl_url
     
- //send audio message 
+ //send audio+ document message 
 
  await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+ await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:">Thenula Panapiti"},{quoted:mek})
 
  
 
+
+}catch(e){
+  console.log(e)
+  reply(`${e}`)
+}
+})
+
+//--------------------------------------VIDEO-DOWNLOAD------------------------------------------------
+
+cmd({
+    pattern: "video",
+    desc: "download videos.",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply(">🔗Please give me a URL or TITLE🔗*")
+const search = await yts (q) 
+const data = search.videos[0];
+const url = data.url
+
+let desc = `
+  *♻️ 📽️Thenu-MD VIDEO Downloader📽️ ♻️*
+  
+  Title: ${data.title}
+  Description: ${data.description}
+  Time: ${data.timestamp}
+  Ago: ${data.ago}
+  Views: ${data.views}
     
+ 💻_video downloading.._
+>© Made by Thenula Panapiti.
+  `
+ await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek})
+ 
+ //download video
+
+ let down = await fg.ytv(url)
+ let downloadUrl = down.dl_url
+    
+ //send video+ document message 
+
+ await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4",caption:">Thenula Panapiti💕"},{quoted:mek})
+ await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:">Thenula Panapiti💕"},{quoted:mek})
+
+ 
+
+
 }catch(e){
   console.log(e)
   reply(`${e}`)
