12eb9ba73c137b6d27f7e2d7fb514acf07b57396
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-08-19 14:38:16 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,24 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
+
+cmd({
+    pattern: "system",
+    alias: ["status","botInfo"],
+    desc: "Check uptime , ram usage and more",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let status = `> ♻️Uptime: ${runtime(process.uptime())} > ♻️Ram usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+> 🔔HostName:* ${os.hostname()} 
+> 🎬Owner:*Thenula Panapiti* ` 
+  return reply(`${status}`)
+  
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
