4a736d69ac8e4c3666c4a43960a84ba1ba3414d1
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-08-21 09:31:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/e1805cc111530fe514728.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/a7ccc1537d4760ce7a0cc.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello!* . 'This is Thenu-MD Whatsapp WA BOT'...I'm alive now.. ..♻️😏..",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public"
