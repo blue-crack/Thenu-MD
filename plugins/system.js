70b710ef729e30ed8ef4581e91ca0a186ba27eec
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-08-21 09:31:46 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -12,7 +12,8 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `> ♻️Uptime: ${runtime(process.uptime())} > ♻️Ram usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+let status = `> ♻️Uptime: ${runtime(process.uptime())} 
+> ♻️Ram usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
 > 🔔HostName:* ${os.hostname()} 
 > 🎬Owner:*Thenula Panapiti* ` 
   return reply(`${status}`)
