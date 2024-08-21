95e036b37c8336a95a23a9528f4566926aaa5a38
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-08-21 08:56:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -165,7 +165,12 @@ mek.type === "stickerMessage"
 ) {
 command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
 }});
-//============================================================================ 
+//======================================WORK-type============================================
+if (!isOwner && config.MODE === "private") return
+if (!isOwner && config.MODE === "inbox") return
+if (!isOwner && !isGroup config.MODE === "groups") return
+//==============================================================================
+        
 
 })
 }
