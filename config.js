4736039f03eed0bb419ed8b5be15cae18e5395bb
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-08-21 08:40:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/e1805cc111530fe514728.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello!* . This is Thenu-MD Whatsapp WA BOT...I'm alive now.. ..♻️😏",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello!* . 'This is Thenu-MD Whatsapp WA BOT'...I'm alive now.. ..♻️😏..",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public"
 
 };
