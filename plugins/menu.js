e1d7f42b3c1da05180a5e43f836c4dddc45fea3c
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-08-22 14:27:14 +0530

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,56 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "menu",
+    desc: "get bot cmd.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menu = {
+ main: '',
+ download: '',
+ group: '',
+ owner: '',
+ convert: '',
+ search: ''
+};
+
+  for (let i = 0; i < commands.length; i++) { if (commands[i].pattern && !commands[i].dontAddCommandList) { menu[commands[i].category] += `.${commands[i].pattern}\n`; } }
+
+ let madeMenu = `💖 *Hello ${pushname}*
+ > *DOWNLOAD COMMANDS📡*
+ 
+ ${menu.download}
+
+ > *MAIN COMMANDS🔰*
+
+ ${menu.main}
+
+ > *GROUP COMMANDS✴️*
+
+ ${menu.group}
+
+ > *OWNER COMMANDS💰*
+
+ ${menu.owner}
+
+ > *CONVERT COMMANDS🚄*
+
+ ${menu.convert}
+
+ > *Search Commands🔎*
+
+ ${menu.search}
+
+ POWERD BY Thenula_Panapiti🔰
+ `
+await conn.sendMessage(from,{text:madeMenu},{quoted:mek})
+
+  }catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
