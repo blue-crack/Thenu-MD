2734512b378265271ce207f26cae38809a7bde4c
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-08-31 16:20:32 +0530

    Create copilot.js

---
+++ plugins/copilot.js
@@ -0,0 +1,19 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const { fetchJson } = require('../lib/functions')
+
+cmd({
+    pattern: "copilot",
+    desc: "copilot chat.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let data = await fetchJson(`https://apis.vihangayt.com/ai/codemirror?q=${q}`)
+return reply(`${data.data}`)
+ }catch(e){
+  console.log(e)
+  reply(`${e}`)
+}
+})
