98e3016003246b26d25e2fd48bf7a4b0df2c821b
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-09-01 12:23:33 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -5,7 +5,7 @@ const { fetchJson } = require('../lib/functions')
 cmd({
     pattern: "ai",
     desc: "ai chat.",
-    category: "main",
+    category: "ai",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
