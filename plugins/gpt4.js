bfa91dfdb49753a75e20a5831e03668deb81d706
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-09-01 20:28:24 +0530

    Create gpt4.js

---
+++ plugins/gpt4.js
@@ -0,0 +1,19 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const { fetchJson } = require('../lib/functions')
+
+cmd({
+    pattern: "gpt4",
+    desc: "gpt4 info.",
+    category: "ai",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let data = await fetchJson(`https://api.vihangayt.com/ai/gpt?q=${q}`)
+return reply(`${data.data}`)
+ }catch(e){
+  console.log(e)
+  reply(`${e}`)
+}
+})
