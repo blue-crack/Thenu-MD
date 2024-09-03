3fcbb45104a1d5b523c2660615f5c49c531ff444
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-09-03 11:38:58 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,7 +10,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const config = await readEnv();
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},{Audio: {url: "https://github.com/prabathLK/AUTO-VOICE-SENDER-PRABATH-MD-/raw/main/plugins/bgm/I%20tried%20so%20hard%20got%20so%20far%20but%20In%20The%20And%20Matter.mp3"},caption: config.ALIVE_MSG},{quoted: mek})
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
