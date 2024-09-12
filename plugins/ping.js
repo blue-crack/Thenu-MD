dafabc8cec0868d42b2076618e1bce0617f07ed6
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-09-12 17:52:04 +0530

    Create ping.js

---
+++ plugins/ping.js
@@ -0,0 +1,22 @@
+const config = require('../config')
+const { cmd, commands } = require('../command')
+
+cmd({
+    pattern: "ping",
+    desc: "Check bot's response time.",
+    category: "main",
+    react: "👾",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const startTime = Date.now()
+        const message = await conn.sendMessage(from, { text: '> THENU-MD...' })
+        const endTime = Date.now()
+        const ping = endTime - startTime
+        await conn.sendMessage(from, { text: `📍 Ping : ${ping}ms`, quoted: message })
+     }catch(e){
+    console.log(e)
+    reply(`${e}`)
+    }
+    })
