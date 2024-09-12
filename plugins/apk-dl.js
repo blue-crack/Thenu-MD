dd5d1672f01e07c9759c307ae7350391bdfb7bf5
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-09-12 20:03:25 +0530

    Create apk-dl.js

---
+++ plugins/apk-dl.js
@@ -0,0 +1,24 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const { fetchJson } = require('../lib/functions')
+
+cmd({
+    pattern: "apkdl",
+    alias: ["modapk"],
+    desc: "download apks",
+    category: "download",
+    filename: __filename,
+    react: "📁"
+},
+async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!q && !q.startsWith("https://")) return reply("𝚊𝚙𝚔 𝚗𝚘𝚝 𝚏𝚘𝚞𝚗𝚍 ‼️")
+        //fetch data from api  
+        let data = await fetchJson(`${baseUrl}/api/apkdl?url=${q}`)
+        reply("*ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ...*")
+        await conn.sendMessage(from, { document: { url: data.data.link_1 }, fileName: data.data.name, mimetype: data.data.file_type, caption: cap }, { quoted: mek })                                                                                                                 
+    } catch (e) {
+        console.log(e)
+        reply(`Cant Find`)
+    }
+})
