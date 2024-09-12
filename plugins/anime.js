c743a9019648509658d1131f9f9aea84947cc953
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-09-12 18:33:02 +0530

    Create anime.js

---
+++ plugins/anime.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const { cmd, commands } = require('../command');
+
+cmd({
+    pattern: "animegirl",
+    desc: "Fetch a random anime girl image.",
+    category: "fun",
+    react: "👧",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const apiUrl = `https://api.waifu.pics/sfw/waifu`;
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👧 *Random Anime Girl Image* 👧\n>  ❯❯ THENU-ᴍᴅ ᴡᴀᴛꜱ ᴀᴘᴘ ʙᴏᴛ ➣' }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`*Error Fetching Anime Girl image*: ${e.message}`);
+    }
+});
