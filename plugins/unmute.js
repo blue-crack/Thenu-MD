d0fca6513b6225b7659d4f110bf6193ae948c866
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-09-12 18:45:43 +0530

    Create unmute.js

---
+++ plugins/unmute.js
@@ -0,0 +1,26 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+cmd({
+     pattern: "unmute",	
+     alias: ["unlock"],
+     react: "🔊",
+     desc: "mute group.",
+     category: "group",
+     filename: __filename,
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants,  isItzcp, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+    if (!isOwner || !isAdmins) return;  
+
+
+    if (!m.isGroup) return reply(mg.onlygroup);
+    if (!isBotAdmins) return reply(mg.needbotadmins);     
+
+            await conn.groupSettingUpdate(m.chat, "not_announcement")
+           const mass = await conn.sendMessage(m.chat, { text: '*Group chat unmuted* 🔊' }, { quoted: mek });
+            return await conn.sendMessage(m.chat, { react: { text: '🔊', key: mass.key } });
+} catch(e) {
+console.log(e);
+reply('*Error !!*')     
+} 
+})
